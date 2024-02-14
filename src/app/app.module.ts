18277b78abb420d25d4982d8fc105875f6969865
Borcze Angelov <Borcze.Angelov@yahoo.com>
2024-02-14 21:21:06 +0100

    refac: add namespace oauth-flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { OauthCallbackComponent } from './oauth-callback/oauth-callback.component';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { OauthCallbackComponent } from './oauth-flow/oauth-callback/oauth-callback.component';
+import { SignInComponent } from './oauth-flow/sign-in/sign-in.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
--- src/app/oauth-flow/sign-in/sign-in.component.ts
+++ src/app/oauth-flow/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from '../oauth-callback/auth.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-sign-in',
